06b044dde65712915fc881abf9bb2248d6cd0209
YinengXiong <1353470@tongji.edu.cn>
2019-09-19 22:54:22 +0800

    add resblock

--- model/basic.py
+++ model/basic.py
@@ -21,3 +21,31 @@ class BasicBlock(nn.Sequential):
         if act is not None:
             m.append(act)
         super(BasicBlock, self).__init__(*m)
+
+class ResBlock(nn.Module):
+    def __init__(self, conv, num_features, kernel_size, stride=1, groups=1, bias=True,
+                 norm=None, act=nn.ReLU(True), res_scale=1):
+        super(ResBlock, self).__init__()
+
+        m = []
+        for i in range(2):
+            m.append(nn.Conv2d(num_features, num_features, kernel_size,
+                               stride=stride, groups=groups, bias=bias))
+            if norm == 'BN':
+                m.append(nn.BatchNorm2d(num_features))
+            elif norm == 'IN':
+                m.append(nn.InstanceNorm2d(num_features))
+            else:
+                pass
+
+            if i == 0:
+                m.append(act)
+
+        self.body = nn.Sequential(*m)
+        self.res_scale = res_scale
+
+    def forward(self, x):
+        res = self.body(x).mul(self.res_scale)
+        res += x
+
+        return res
