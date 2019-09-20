261ef21d12b51abe6192822c7167bb8d824606a3
YinengXiong <1353470@tongji.edu.cn>
2019-09-20 17:23:39 +0800

    modify basic

--- model/basic.py
+++ model/basic.py
@@ -4,19 +4,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def get_norm(num_features, norm_type):
+    if norm_type == 'BN':
+        return nn.BatchNorm2d(num_features)
+    elif norm_type == 'IN':
+        return nn.InstanceNorm2d(num_features)
+
 class BasicBlock(nn.Sequential):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, groups=1, bias=True,
-                 norm=None, act=nn.ReLU(True)):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=1,
+                 groups=1, bias=True, norm=None, act=nn.ReLU(True)):
 
-        m = [nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, groups=groups,
-                       bias=bias)]
-
-        if norm == 'BN':
-            m.append(nn.BatchNorm2d(out_channels))
-        elif norm == 'IN':
-            m.append(nn.InstanceNorm2d(out_channels))
-        else:
-            pass
+        m = [nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding,
+                       groups=groups, bias=bias)]
+        if norm is not None:
+            m.append(get_norm(out_channels, norm))
 
         if act is not None:
             m.append(act)
@@ -31,12 +32,8 @@ class ResBlock(nn.Module):
         for i in range(2):
             m.append(nn.Conv2d(num_features, num_features, kernel_size,
                                stride=stride, groups=groups, bias=bias))
-            if norm == 'BN':
-                m.append(nn.BatchNorm2d(num_features))
-            elif norm == 'IN':
-                m.append(nn.InstanceNorm2d(num_features))
-            else:
-                pass
+            if norm is not None:
+                m.append(get_norm(num_features, norm))
 
             if i == 0:
                 m.append(act)
@@ -49,3 +46,58 @@ class ResBlock(nn.Module):
         res += x
 
         return res
+
+def Upblock2x(direction, num_features, upsample_type, bias):
+    m = []
+    if direction == 0:
+        k1, k2 = 1, 4
+        s1, s2 = 1, 2
+        p1, p2 = 0, 1
+    elif direction == 1:
+        k1, k2 = 4, 1
+        s1, s2 = 2, 1
+        p1, p2 = 1, 0
+    else:
+        k1, k2 = 4, 4
+        s1, s2 = 2, 2
+        p1, p2 = 1, 1
+
+    if upsample_type == 'deconv':
+        m.append(nn.Conv2d(num_features, num_features, kernel_size=3, stride=1, padding=1, bias=bias))
+        m.append(nn.ConvTranspose2d(num_features, num_features, kernel_size=(k1, k2),
+                                    stride=(s1, s2), padding=(p1, p2)))
+    else:
+        m.append(nn.Conv2d(num_features, num_features * 4, kernel_size=3, stride=1, padding=1, bias=bias))
+        m.append(nn.PixelShuffle(2))
+    return nn.Sequential(*m)
+
+class Upsample(nn.Sequential):
+    def __init__(self, scale, direction, num_features, upsample_type='deconv',
+                 norm=None, act=nn.ReLU(True), bias=True):
+        m = []
+        if (scale & (scale - 1)) == 0:
+            for _ in range(int(math.log(scale, 2))):
+                # X2 block
+                m += Upblock2x(direction, num_features, upsample_type, bias)
+
+                if norm is not None:
+                    m.append(get_norm(num_features, norm))
+
+                if act is not None:
+                    m.append(act)
+
+        elif scale == 3 and upsample_type != 'deconv' and direction == 2:
+            # X3 block only support pixelshuffle now
+            m.append(nn.Conv2d(num_features, 9 * num_features, kernel_size=3, stride=1, padding=1, bias=bias))
+            m.append(nn.PixelShuffle(3))
+
+            if norm is not None:
+                m.append(get_norm(num_features, norm))
+
+            if act is not None:
+                m.append(act)
+
+        else:
+            raise NotImplementedError
+        super(Upsample, self).__init__(*m)
+
